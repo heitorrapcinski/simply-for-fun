732f0169537c9ea45efc23a79125e3612dfb8e15
Heitor Rapcinski <heitor.rapcinski@gmail.com>
2025-02-20 10:30:05 -0300

    update

--- docker/ambiente-jboss-mssql/images/build.ps1
+++ docker/ambiente-jboss-mssql/images/build.ps1
@@ -1,2 +1,4 @@
+Copy-Item "~\Bin\jboss*.zip" -Destination ".\bin"
+
 docker build -t alpinelinux-jboss:latest -f .\setup\alpinelinux-jboss\Dockerfile .
 docker build -t oraclelinux-jboss:latest -f .\setup\oraclelinux-jboss\Dockerfile .
