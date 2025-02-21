4af1a2193c838bca186479d507c011502adf8192
Heitor Rapcinski <heitor.rapcinski@gmail.com>
2025-02-21 11:53:03 -0300

    update

--- docker/ambiente-jboss-mssql/deploy.ps1
+++ docker/ambiente-jboss-mssql/deploy.ps1
@@ -1,5 +1,8 @@
+#create database
+docker exec ambiente-jboss-mssql-database-mssqlserver-1 /opt/mssql-tools18/bin/sqlcmd -No -H  database-mssqlserver -U sa -P P@ssW0rd -i /tmp/instnwnd.sql
+
 #config JBoss
 docker exec ambiente-jboss-mssql-middleware-jboss-1 /opt/jboss-eap-7.4/bin/jboss-cli.sh --file=/tmp/jboss-config.cli
 
-#create database
-docker exec ambiente-jboss-mssql-database-mssqlserver-1 /opt/mssql-tools18/bin/sqlcmd -No -H  database-mssqlserver -U sa -P P@ssW0rd -i /tmp/instnwnd.sql
+#use /opt/jboss-eap-7.4/bin/add-user.sh to add admin user
+
