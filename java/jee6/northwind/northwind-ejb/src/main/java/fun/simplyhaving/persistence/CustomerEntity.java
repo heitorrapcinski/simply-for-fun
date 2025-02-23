d99ebedb4948a8722d51855d751eb7402baa62da
Heitor Rapcinski <heitor.rapcinski@gmail.com>
2025-02-23 12:17:32 -0300

    update

--- java/jee6/northwind/northwind-ejb/src/main/java/fun/simplyhaving/persistence/CustomerEntity.java
+++ java/jee6/northwind/northwind-ejb/src/main/java/fun/simplyhaving/persistence/CustomerEntity.java
@@ -16,20 +16,27 @@ public class CustomerEntity extends Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    @Column(nullable = false, length = 30)
+    @Column(nullable = false, length = 40)
     @NotNull
-    @Size(min = 1, max = 30)
+    @Size(min = 1, max = 40)
     private String companyName;
+    @Column(length = 30)
     private String contactTitle;
+    @Column(length = 30)
     private String contactName;
+    @Column(length = 60)
     private String address;
+    @Column(length = 15)
     private String city;
+    @Column(length = 15)
     private String region;
+    @Column(length = 10)
     private String postalCode;
+    @Column(length = 15)
     private String country;
+    @Column(length = 24)
     private String phone;
+    @Column(length = 24)
     private String fax;    
-
-
     
 }
