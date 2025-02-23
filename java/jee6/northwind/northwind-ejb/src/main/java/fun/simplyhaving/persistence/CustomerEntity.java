27b6c2e56fd64d0ac4d7be6845974dd717d01aee
Heitor Rapcinski <heitor.rapcinski@gmail.com>
2025-02-23 12:02:22 -0300

    update

--- java/jee6/northwind/northwind-ejb/src/main/java/fun/simplyhaving/persistence/CustomerEntity.java
+++ java/jee6/northwind/northwind-ejb/src/main/java/fun/simplyhaving/persistence/CustomerEntity.java
@@ -1,11 +1,34 @@
 package fun.simplyhaving.persistence;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import fun.simplyhaving.domain.Customer;
 
 @Entity
 public class CustomerEntity extends Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(nullable = false, length = 30)
+    @NotNull
+    @Size(min = 1, max = 30)
+    private String companyName;
+    private String contactTitle;
+    private String contactName;
+    private String address;
+    private String city;
+    private String region;
+    private String postalCode;
+    private String country;
+    private String phone;
+    private String fax;    
 
 
     
