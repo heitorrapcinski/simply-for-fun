66af72967784116c6bfbd9de890cd27b8ef61bb5
Heitor Rapcinski <heitor.rapcinski@gmail.com>
2025-02-24 19:41:23 -0300

    update

--- java/jee6/northwind/northwind-interface-rest/src/main/java/fun/simplyhaving/inbound/Catalog.java
+++ java/jee6/northwind/northwind-interface-rest/src/main/java/fun/simplyhaving/inbound/Catalog.java
@@ -9,7 +9,6 @@ import javax.ejb.Stateless;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.UriInfo;
@@ -29,14 +28,9 @@ public class Catalog implements Serializable {
 
     @GET
     @Path("/customers")
-    @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
     public List<Customer> getAllCustomers() {
-        List<Customer> list = customerService.getAllCustomers();
-
-        System.out.println(list.get(0).getId());
-
-        return list;
+        return customerService.getAllCustomers();
     }
     
 }
